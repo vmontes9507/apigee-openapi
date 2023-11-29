74969d9e744dde0d996db5f9ad20dfa75901d865
victor.montes@digitalfemsa.com <victor.montes@digitalfemsa.com>
2023-11-29 16:11:01 -0600

    Added to stoplight

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyODAzNg/spectral.js?branch_slug=main&token=6018d37f-d581-4470-aaa4-01844c486fae";
+export default { extends: ruleset };
